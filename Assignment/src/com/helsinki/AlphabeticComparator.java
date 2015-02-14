617a2f85368f385975eb4e6516033bd7dd397768
shubham keshri <shubhamkeshri@gmail.com>
2015-02-15 03:00:03 +0530

    Adding Comparator file

---
+++ Assignment/src/com/helsinki/AlphabeticComparator.java
@@ -0,0 +1,22 @@
+package com.helsinki;
+
+import java.util.Comparator;
+
+/**
+ * The Class AlphabeticComparator, a comparator class to compare two strings
+ * alphabetically.
+ */
+public class AlphabeticComparator implements Comparator {
+
+	/*
+	 * Method compare which returns negative,zero or positive integer if string string2 is alphabetically 
+	 */
+	@Override
+	public int compare(Object arg0, Object arg1) {
+
+		InputString inputString1 = (InputString) arg0;
+		InputString inputString2 = (InputString) arg1;
+		return inputString1.inString.compareTo(inputString2.inString);
+	}
+
+}
